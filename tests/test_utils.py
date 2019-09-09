63ae01954df3b2bebd4dc7e91f743942b0687f1b
vainerj <jan.vainer@merck.com>
2019-09-09 16:16:45 +0200

    Utils: Add TensorQueue and its test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -5,7 +5,7 @@ abs_dir = os.path.dirname(os.path.abspath(__file__)) # abs adress of this file
 root_folder = abs_dir.split('tests')[0]
 sys.path.append(root_folder)
 
-from models.utils import random_chunks
+from utils.utils import random_chunks, TensorQueue
 
 
 def test_random_chunks():
@@ -14,3 +14,21 @@ def test_random_chunks():
     for i in range(10):
         for chunk in random_chunks(lst, lower, upper):
             assert lower <= len(chunk) <= upper
+
+
+def test_tensor_queue():
+    import torch
+    queue = TensorQueue(5)
+    a = torch.Tensor([1,2,3])
+    b = torch.Tensor([4, 5, 6, 7, 8, 9])
+    queue.push(a)
+    assert torch.all(torch.eq(queue.queue, a))
+    queue.push(b[:1])
+    assert torch.all(torch.eq(queue.queue, torch.Tensor([1,2,3,4])))
+    queue.push(b)
+    assert torch.all(torch.eq(queue.queue, torch.Tensor([5, 6, 7, 8, 9])))
+    x = queue.push(a)
+    assert torch.all(torch.eq(queue.queue, torch.Tensor([8, 9, 1, 2, 3])))
+    assert torch.all(torch.eq(x, torch.Tensor([5, 6, 7])))
+    queue.pop(2)
+    assert torch.all(torch.eq(queue.queue, a))
