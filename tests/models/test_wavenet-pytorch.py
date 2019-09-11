dea64bc4af09089bb7c0dafbbbd4ad36e8e0cf8a
vainerj <jan.vainer@merck.com>
2019-09-11 14:48:23 +0200

    Wave-net: Make WaveGenerator work - needs testing

--- tests/models/test_wavenet-pytorch.py
+++ tests/models/test_wavenet-pytorch.py
@@ -8,14 +8,14 @@ abs_dir = os.path.dirname(os.path.abspath(__file__)) # abs adress of this file
 root_folder = abs_dir.split('tests')[0]
 sys.path.append(root_folder)
 
-from models.wave_net_pytorch import WaveNet, WaveBlock
+from models.wave_net_pytorch import WaveNet, WaveBlock, WaveGenerator
 
 
 # TODO: write test class for WaveNet
 
 dilations = [2**i for j in range(5) for i in range(5)]
 
-net = WaveNet(filters=15,
+net = WaveNet(filters=3,
               kernel_size=2,
               dilations=dilations,
               categories=3,
@@ -28,14 +28,22 @@ batch = torch.Tensor([[[0, 0, 0, 1],
                        [0, 1, 0, 0]],
                       [[1, 1, 0, 0],
                        [0, 0, 0, 1],
-                       [0, 0, 1, 0]]]).type(torch.cuda.FloatTensor)
-target = torch.Tensor([[1, 2, 1, 0], [0, 0, 2, 1]]).type(torch.cuda.LongTensor)
+                       [0, 0, 1, 0]]]).type(torch.FloatTensor)
+target = torch.Tensor([[1, 2, 1, 0], [0, 0, 2, 1]]).type(torch.LongTensor)
+
+#print(batch.type())
+#print(net.one_hot([1]).unsqueeze(0).shape)
+#print(net(batch[:1], probs=True)[0, :,-1])
+#print(torch.multinomial(net(batch, probs=True)[0, :,-1], 1))
+#gen = WaveGenerator(net, [1, 2, 1, 0])
+
+#net.generate([1,2,1,0], 1000)
+#for i in range(10000):
+#   print('{}: {}'.format(i, next(gen)))
 
 criterion = nn.CrossEntropyLoss()  # only accepts logits!
 optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-# FIXME: gradient randomly explodes and loss gets reeaally large and then net outputs nan.
-# - try grad cliping
 
 for i in range(1000):
     inputs, targets = batch[:, :, :-1], target[:, 1:]
@@ -44,7 +52,6 @@ for i in range(1000):
     outputs = net(inputs, probs=False)  # output logits
     loss = criterion(outputs, targets)
     loss.backward()
+    torch.nn.utils.clip_grad_norm_(net.parameters(), 1)
     optimizer.step()
     print(loss.item())
-
-# loss decreases
